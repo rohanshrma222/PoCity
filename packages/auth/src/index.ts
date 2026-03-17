a9eefbed8cddeef3a2a3797c0582aa41c1d63eb3
rohanshrma222 <hackerviruse04@gmail.com>
2026-03-17 23:24:06 +0530

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import prisma from "@PoCity/db";
+import { env } from "@PoCity/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
